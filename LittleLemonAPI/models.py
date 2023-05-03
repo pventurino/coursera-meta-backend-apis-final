c769b0f197a38b0f40cc442f678bb394604c41ca
Pablo Venturino <pablojventurino@gmail.com>
2023-05-02 19:09:13 -0700

    include order items on /orders response

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -49,7 +49,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     """Each menu item in an order."""
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     quantity = models.SmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -22,7 +22,13 @@ class CartItemSerializer(serializers.ModelSerializer):
             'price': {'read_only': True},
         }
 
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = OrderItem
+        fields = ('__all__')
+
 class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
     class Meta():
         model = Order
         fields = ('__all__')
